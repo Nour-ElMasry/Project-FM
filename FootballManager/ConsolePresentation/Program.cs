3f18f27e31a7df478d79d8c76f6fcddc91f00915
Elafifi Nour-Eldin <59283990+Nour-ElMasry@users.noreply.github.com>
2022-08-05 13:32:49 +0300

    consoleApp modification

--- FootballManager/ConsolePresentation/Program.cs
+++ FootballManager/ConsolePresentation/Program.cs
@@ -1,16 +1,1 @@
-﻿using Domain;
-using Domain.Entities;
-
-try
-{
-    HumanManager p = new HumanManager("SlimFiFi01","A7vcsdv$","nour", "2001-02-18", "Egyptian", "D:\\Stuff\\Work-stuff\\Project\\FootballManagerDomainModel.png");
-    Team rm = new Team("RMA", "Spain", "Santiago bernabeu", p);
-
-    Console.WriteLine(rm.Name + "\n" + rm.Manager);
-}
-catch (Exception e)
-{
-    Console.WriteLine($"Error: {e.Message}");
-}
-
-Console.ReadKey();
+﻿Console.ReadKey();
