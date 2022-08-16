56178d9e5158608b15a0ff2fbc68160f2a43a9d8
Nour-ElMasry <noure5046@gmail.com>
2022-08-16 10:54:15 +0300

    Fixture class modification

--- FootballManager/Domain/Entities/FixtureContainer/Fixture.cs
+++ FootballManager/Domain/Entities/FixtureContainer/Fixture.cs
@@ -1,8 +1,10 @@
-﻿using Domain.Entities.TeamContainer;
+﻿using Domain.Entities.LeagueContainer;
+using Domain.Entities.TeamContainer;
 
 namespace Domain.Entities.FixtureContainer;
 public class Fixture
 {
+    public League League { get; set; }
     public string Venue { get; set; }
     public DateTime Date { get; set; }
     public Team HomeTeam { get; set; }
@@ -10,8 +12,9 @@ public class Fixture
     public int HomeTeamScore { get; set; }
     public int AwayTeamScore { get; set; }
 
-    public Fixture(string venue, DateTime date, Team homeTeam, Team awayTeam)
+    public Fixture(League league, string venue, DateTime date, Team homeTeam, Team awayTeam)
     {
+        League = league;
         Venue = venue;
         Date = date;
         HomeTeam = homeTeam;
